49bfc4c208ec4a3b784064abc6786295361a4fd7
Belengiri <147511270+Belengiri@users.noreply.github.com>
2023-11-06 19:35:41 -0300

    vista medicos andando con errores

--- PracticaTrabajoFinal/Controladores/ControladoraMedicos.cs
+++ PracticaTrabajoFinal/Controladores/ControladoraMedicos.cs
@@ -1,12 +1,89 @@
-﻿using System;
+﻿using PracticaTrabajoFinal.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PracticaTrabajoFinal.Controladores
 {
     public class ControladoraMedicos
     {
+        Medicos medico = new Medicos();
+        public void Agregar_Servicio(string nombre)
+        {
+            try
+            {
+                if (nombre != "")
+                {
+                    medico.agregarservicio(nombre);
+                }
+                else
+                {
+                    MessageBox.Show("campos vacios");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error al agregar un Servicio", ex.Message);
+            }
+        }
+        public void Agregar_Medico(int id_servicio, string nombre, string apellido, int matricula)
+        {
+            try
+            {
+                if (id_servicio != 0 && nombre != "" && apellido != "" && matricula != 0)
+                {
+                    medico.agregarmedico(id_servicio, nombre, apellido, matricula);
+                }
+                else
+                {
+                    MessageBox.Show("campos vacios");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error al agregar un Medico", ex.Message);
+            }
+        }
+        public void Modificar_Medico(int id_medico, int id_servicio, string nombre, string apellido, int matricula)
+        {
+            try
+            {
+                if (id_medico != 0 && nombre != "" && id_servicio != 0 && apellido != "" && matricula != 0)
+                {
+                    medico.modificarmedico(id_medico, id_servicio, nombre, apellido, matricula);
+                }
+                else
+                {
+                    MessageBox.Show("campos vacios");
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("error al modificar un Medico", e.ToString());
+            }
+        }
+        public void Eliminar_Medico(int id_medico)
+        {
+            try
+            {
+                if (id_medico != 0)
+                {
+                    medico.eliminarmedico(id_medico);
+                }
+                else
+                {
+                    MessageBox.Show("campos vacios");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("error al eliminar un Medico", e.ToString());
+            }
+        }
     }
 }
